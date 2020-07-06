19fb9a84a5c5dbfb95945c518c73ca7e1d20a284
canyon <gongyanghui1986@163.com>
2020-07-06 14:38:40 +0800

    添加取得AssetInfo的东西

--- Assets/Lua/games/logics/mgr_res.lua
+++ Assets/Lua/games/logics/mgr_res.lua
@@ -10,20 +10,28 @@ local this = M
 
 function M.Init()
 	this._InitLoadFuncs()
+	this._GetAssetFuncs()
 end
 
 function M._InitLoadFuncs()
-	local _lb  = this.lbLoadFuncs or {}
-	this.lbLoadFuncs = _lb
+	local _lb  = this.lbLoads or {}
+	this.lbLoads = _lb
 	_lb[LE_AsType.Fab] = this._LoadFab;
 	_lb[LE_AsType.Sprite] = this._LoadSprite;
 	_lb[LE_AsType.Texture] = this._LoadTexture;
 end
 
+function M._GetAssetFuncs()
+	local _lb  = this.lbGets or {}
+	this.lbGets = _lb
+	_lb[LE_AsType.Fab] = this._Get4Fab;
+	_lb[LE_AsType.Sprite] = this._Get4Sprite;
+	_lb[LE_AsType.Texture] = this._Get4Texture;
+end
 
 function M._LoadFab(abName,assetName,callLoad)
 	CResMgr.LoadFab(abName,assetName,function(obj)
-		printInfo("== [%s] == [%s] == [%s] ",abName,assetName,obj)
+		-- printInfo("== [%s] == [%s] == [%s] ",abName,assetName,obj)
 		if callLoad then
 			callLoad(obj);
 		end
@@ -32,7 +40,7 @@ end
 
 function M._LoadSprite(abName,assetName,callLoad)
 	CResMgr.LoadSprite(abName,assetName,function(obj)
-		printInfo("== [%s] == [%s] == [%s] ",abName,assetName,obj)
+		-- printInfo("== [%s] == [%s] == [%s] ",abName,assetName,obj)
 		if callLoad then
 			callLoad(obj);
 		end
@@ -41,7 +49,7 @@ end
 
 function M._LoadTexture(abName,assetName,callLoad)
 	CResMgr.LoadTexture(abName,assetName,function(obj)
-		printInfo("== [%s] == [%s] == [%s] ",abName,assetName,obj)
+		-- printInfo("== [%s] == [%s] == [%s] ",abName,assetName,obj)
 		if callLoad then
 			callLoad(obj);
 		end
@@ -49,7 +57,7 @@ function M._LoadTexture(abName,assetName,callLoad)
 end
 
 function M.LoadAsset(abName,assetName,assetLType,callLoad)
-	local _func = this.lbLoadFuncs[assetLType];
+	local _func = this.lbLoads[assetLType];
 	if _func then
 		_func(abName,assetName,callLoad);
 	else
@@ -62,6 +70,27 @@ function M.UnLoad(abName,assetName,assetLType)
 		CResMgr.UnLoadFab(abName,assetName);
 	else
 		CResMgr.UnLoadAsset(abName);
+	end
+end
+
+function M._Get4Fab(abName,assetName)
+	return CResMgr.GetAsset4Fab(abName,assetName)
+end
+
+function M._Get4Sprite(abName,assetName)
+	return CResMgr.GetAsset4Fab(abName,assetName)
+end
+
+function M._Get4Texture(abName,assetName)
+	return CResMgr.GetAsset4Fab(abName,assetName)
+end
+
+function M.GetAsset(abName,assetName,assetLType)
+	local _func = this.lbGets[assetLType];
+	if _func then
+		return _func(abName,assetName);
+	else
+		printError("get assetinfo err by type, abName =[%s],assetName =[%s],aLtype =[%s]",abName,assetName,assetLType);
 	end
 end
 
