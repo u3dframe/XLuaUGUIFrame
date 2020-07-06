05a888c8e8bee63556bc1efaace2008b63a460af
canyon <gongyanghui1986@163.com>
2020-07-06 14:37:10 +0800

    更新事件

--- Assets/Lua/games/defines/define_events.lua
+++ Assets/Lua/games/defines/define_events.lua
@@ -1,4 +1,5 @@
 ------------- event names 消息定义
+Evt_Update = "Evt_Update"; -- update 分针更新函数
 Evt_ToView_UpRes = "Evt_ToView_UpRes"; -- 更新界面
 Evt_ToView_Login = "Evt_ToView_Login"; -- 登录界面
 Evt_SceneLoaded = "Evt_SceneLoaded"; -- 场景切换事件(level)
