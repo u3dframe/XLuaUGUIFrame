6b5a312a707eb5895e2363806f8f98d5502ca6d0
canyon <gongyanghui1986@163.com>
2020-07-07 10:56:36 +0800

    添加函数读取

--- Assets/Lua/games/basics/uc_fabelement.lua
+++ Assets/Lua/games/basics/uc_fabelement.lua
@@ -4,11 +4,40 @@
 	-- Date : 2020-06-27 13:25
 	-- Desc : 
 ]]
+local str_format = string.format
 local super = LuCFabBasic
 local M = class( "lua_PrefabElement",super )
 
 function M:ctor( obj )
 	super.ctor(self,obj,"PrefabElement")
+end
+
+function M:IsHasGobj( elName )
+	local _k = str_format("__isHas_%s",elName)
+	if not self[_k] then 
+		self[_k] = self.comp:IsHasGobj(elName); 
+	end
+	return self[_k]
+end
+
+function M:GetGobjElement( elName )
+	local _k = str_format("__gobj_%s",elName)
+	if not self[_k] then 
+		self[_k] = self.comp:GetGobjElement(elName); 
+	end
+	return self[_k]
+end
+
+function M:GetComponent4Element( elName,strComp )
+	local _k = str_format("__com_%s_%s",elName,strComp)
+	if not self[_k] then 
+		self[_k] = self.comp:GetComponent4Element(elName); 
+	end
+	return self[_k]
+end
+
+function M:SetActive4Element( elName,isActive )
+	self.comp:SetActive(elName,isActive == true);
 end
 
 return M
