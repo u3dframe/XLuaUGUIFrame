4c151ac3c5bcb91010b3714d1529663ed5a1146d
canyon <gongyanghui1986@163.com>
2020-07-13 14:25:33 +0800

    添加代码备份

---
+++ _backups/AudioManager.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using Core;
+
+public class AudioManager : MonoBehaviour {
+	public AudioSource m_bgAudio;
+	public AudioSource m_uiAudio;
+	[Range(0f,1f)] public float bgSceneVolume = 1; // 场景背景音乐大小
+	[Range(0f,1f)] public float bgUIVolume = 1; // UI背景音乐大小
+	[Range(0f,1f)] protected float soundVolume = 0.1f; // 短声音的大小
+	
+	Dictionary<string,AssetInfo> m_clips = new Dictionary<string,AssetInfo>();
+}
+
+
---
+++ _backups/GobjNotDestroy.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+
+/// <summary>
+/// 类名 : GameObject 不能被消耗
+/// 作者 : Canyon
+/// 日期 : 2017-03-21 10:37
+/// 功能 : 
+/// </summary>
+public class GobjNotDestroy : MonoBehaviour {
+	static public GobjNotDestroy Get(GameObject gobj,bool isAdd){
+		GobjNotDestroy _r = gobj.GetComponent<GobjNotDestroy> ();
+		if (isAdd && null == _r) {
+			_r = gobj.AddComponent<GobjNotDestroy> ();
+		}
+		return _r;
+	}
+
+	static public GobjNotDestroy Get(GameObject gobj){
+		return Get(gobj,true);
+	}
+
+	void Awake()
+	{
+		GameObject.DontDestroyOnLoad(this.gameObject);
+	}
+}
