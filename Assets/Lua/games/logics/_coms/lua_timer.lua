aab49b405c13cbe60adc4faf8896124806dd5966
龚阳辉 <gongyanghui@dianyuegame.com>
2020-07-24 15:35:31 +0800

    完善timer对象

--- Assets/Lua/games/logics/_coms/lua_timer.lua
+++ Assets/Lua/games/logics/_coms/lua_timer.lua
@@ -1,21 +1,34 @@
 --[[
-	-- 游戏定时操作
+	-- 定时及服务器时间管理操作
 	-- Author : canyon / 龚阳辉
 	-- Date   : 2020-07-23 12:20
 	-- Desc   : 延迟执行,每秒及定点通知
 ]]
 
 local tb_insert = table.insert
+local tb_cid = table.contains_id
+local tb_rm = table.removeValuesFunc
+local tb_for = table.foreachArrs
 local _tEx,_nEx = TimeEx,NumEx
+local _dTr,_pErr,_clr = debug.traceback,printError,clearLT
+local _tLb,_ok,_err = {}
 
 local super,_evt = LuaObject,Event
 local M = class( "lua_timer",super )
 local this = M
 
 function M.Init()
+	this:clean_end()
+end
+
+function M:clean_end()
+	this.oneMin = 60
+	this.oneHour = 60 * 24
 	this.sumCDTime = 0
 	this.gcTime = 0
 	this.cdSec = 1
+	this.cdMin = this.oneMin
+	this.cdHour = this.oneHour
 	this:ReEvent4OnUpdate(true)
 end
 
@@ -23,17 +36,38 @@ function M:OnUpdate(dt)
 	this.sumCDTime = this.sumCDTime + dt
 	this.gcTime = this.gcTime + dt
 	this.cdSec = this.cdSec - dt
+	this.cdMin = this.cdMin - dt
+	this.cdHour = this.cdHour - dt
+
 	if this.cdSec <= 0 then
 		this.cdSec = this.cdSec + 1
 		_evt.Brocast(Evt_UpEverySecond)
 	end
 
+	if this.cdMin <= 0 then
+		this.cdMin = this.cdMin + this.oneMin
+		this.curMin = (this.curMin + 1) % 60
+		_evt.Brocast(Evt_UpEveryMin,this.curMin)
+	end
+
+	if this.cdHour <= 0 then
+		this.cdHour = this.cdHour + this.oneHour
+		this.curHour = (this.curHour + 1) % 24
+		_evt.Brocast(Evt_UpEveryHour,this.curHour)
+	end
+
+	this._ExcDelayFunc(dt)
+
 	if this.gcTime >= 900 then
 		this.gcTime = this.gcTime - 900
-		CHelper.ClearMemory()
-		collectgarbage("collect")
-		collectgarbage()
+		this.GC()
 	end
+end
+
+function M.GC()
+	CHelper.ClearMemory()
+	collectgarbage("collect")
+	collectgarbage()
 end
 
 function M.SetSvTimeMs(svMs)
@@ -49,7 +83,13 @@ function M.SetSvTime(svTimeSec)
 		this.sumCDTime = this.sumCDTime + this.lastSvSec - _svSec
 	end
 	this.lastSvSec = _svSec
-	this.cdSec = this.cdSec - _nEx.modDecimal(_svSec)
+	this.cdSec = 1 - _nEx.modDecimal(_svSec)
+	_svSec = (this.cdSec == 1) and 0 or this.cdSec
+	local _lb = this.getDate()
+	this.curMin = _lb.min
+	this.curHour = _lb.hour
+	this.cdMin = (this.oneMin - _lb.sec) + _svSec
+	this.cdHour = (this.oneHour - this.curMin * this.oneMin) + this.cdMin
 	this.isUping = true
 end
 
@@ -65,8 +105,62 @@ function M.GetSvDate()
 	return _tEx.getDate()
 end
 
-function M.AddDelayFunc(cmd,delay,func)
+local function _rf_delay(item,obj)
+	return item.cmd == obj
+end
+
+function M.RemoveDelayFunc(cmd,isNoAll)
+	local times = (isNoAll == true) and 1 or -1
+	tb_rm(this._lbFuncDelays,_rf_delay,cmd,times)
+end
+
+function M.AddDelayFunc(cmd,delay,func,loop)
+	this._lbFuncDelays = this._lbFuncDelays or {}
+	local _v = table.getVK(this._lbFuncDelays,"cmd",cmd)
+	loop = (loop or 1)
+	if _v and _v.delay > 0.01 then
+		_v.delay = delay
+		_v.func = func
+		_v.loop = loop -- 负数标识无线循环
+	else
+		tb_insert(this._lbFuncDelays,{cmd = cmd,delay = delay,func = func,loop = loop})
+	end
+end
+
+function M._ExcDelayFunc(dt)
+	for _, v in ipairs(this._lbFuncDelays) do
+		if v.delay > 0 then
+			v.delay = v.delay - dt
+		else
+			v.loop = v.loop - 1
+			if v.func then
+				_ok,_err = xpcall(v.func,_dTr)
+				if not _ok then
+					v.loop = 0
+					_pErr(_err)
+				end
+			else
+				v.loop = 0
+			end
+
+			if v.loop == 0 then
+				tb_insert(_tLb,v.cmd)
+			end
+		end
+	end
 	
+	if #_tLb <= 0 then return end
+
+	for _, v in ipairs(_tLb) do
+		this.RemoveDelayFunc(v)
+	end
+
+	_clr(_tLb)
+end
+
+function M.OutGame()
+	this.isUping = false
+	this:clean()
 end
 
 return M
