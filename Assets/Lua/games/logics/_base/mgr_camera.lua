ba28c74f53c6e249127fd13c758f1cb1d4a30fab
龚阳辉 <gongyanghui@dianyuegame.com>
2020-07-24 16:15:06 +0800

    添加取得主摄像机的代码

--- Assets/Lua/games/logics/_base/mgr_camera.lua
+++ Assets/Lua/games/logics/_base/mgr_camera.lua
@@ -9,11 +9,11 @@ local super,_evt,_base = MgrBase,Event,SceneBase
 local M = class( "mgr_camera",super )
 
 function M:Init()
-	self._lfVwCamera = handler(self,self.GetCamera)
+	self._lfVwCamera = handler(self,self.GetLuaCamera)
 	_evt.AddListener(Evt_View_MainCamera,self._lfVwCamera);
 end
 
-function M:GetCamera()
+function M:GetLuaCamera()
 	if self.lbCamera then return self.lbCamera end
 	self.lbCamera = _base.New({
 		abName = "m_camera",
@@ -21,12 +21,17 @@ function M:GetCamera()
 		isStay = true,
 	})
 	self.lbCamera.OnInit = function(_s)
+		_s.mainCamera = _s.comp.m_camera
 		_s:SetParent(nil,true)
 		_s:DonotDestory()
 		_s:SetEulerAngles(0,0,0)
 	end
 	self.lbCamera:View(true)
 	return self.lbCamera
+end
+
+function M:GetMainCamera()
+	return self:GetLuaCamera().mainCamera
 end
 
 return M
