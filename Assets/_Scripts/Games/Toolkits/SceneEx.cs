cef8292805fbc20b1b646bc289b2cd7c6cbfe907
canyons <gongyanghui1986@163.com>
2020-08-17 21:40:50 +0800

    完善读写RenderSettings

--- Assets/_Scripts/Games/Toolkits/SceneEx.cs
+++ Assets/_Scripts/Games/Toolkits/SceneEx.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.Rendering;
 using System.Collections;
 
 /// <summary>
@@ -25,8 +26,31 @@ public class SceneEx : MonoBehaviour
 	public float m_fogDensity = 0.0f;
 	public float m_fogStartDistance = 0.0f;
 	public float m_fogEndDistance = 0.0f;
+
+	// Environment 相关
 	public Material m_skybox;
-	
+	public Light m_sunlight;
+
+	// ambient
+	public SphericalHarmonicsL2 m_ambientProbe;
+	public Color m_ambientLight;
+	public Color m_ambientGroundColor;
+	public Color m_ambientEquatorColor;
+	public Color m_ambientSkyColor;
+	public AmbientMode m_ambientMode;
+	public float m_ambientIntensity;
+
+	// reflection
+	public DefaultReflectionMode m_reflectionSource;
+	public int m_defaultReflectionResolution;
+	public int m_reflectionBounces;
+	public float m_reflectionIntensity;
+	public Cubemap m_customReflection;
+
+	// flare
+	public float m_flareStrength;
+	public float m_flareFadeSpeed;
+
 	void Awake () {
         if(Application.isPlaying){
             LoadSetting();
@@ -34,27 +58,79 @@ public class SceneEx : MonoBehaviour
     }
 	
 #if UNITY_EDITOR
-	[ContextMenu("Save Fog")]
+	[ContextMenu("Save RenderSettings")]
 	void _SaveSetting(){
+		_SaveFog();
+		_SaveEnvironment();
+	}
+
+	void _SaveFog(){
 		m_fogEnable = RenderSettings.fog;
 		m_fogMode = RenderSettings.fogMode;
 		m_fogColor = RenderSettings.fogColor;
 		m_fogDensity = RenderSettings.fogDensity;
 		m_fogStartDistance = RenderSettings.fogStartDistance;
 		m_fogEndDistance = RenderSettings.fogEndDistance;
+	}
+
+	void _SaveEnvironment(){
 		m_skybox = RenderSettings.skybox;
+		m_sunlight = RenderSettings.sun;
+
+		m_ambientProbe = RenderSettings.ambientProbe;
+		m_ambientLight = RenderSettings.ambientLight;
+		m_ambientGroundColor = RenderSettings.ambientGroundColor;
+		m_ambientEquatorColor = RenderSettings.ambientEquatorColor;
+		m_ambientSkyColor = RenderSettings.ambientSkyColor;
+		m_ambientMode = RenderSettings.ambientMode;
+		m_ambientIntensity = RenderSettings.ambientIntensity;
+
+		m_reflectionSource = RenderSettings.defaultReflectionMode;
+		m_defaultReflectionResolution = RenderSettings.defaultReflectionResolution;
+		m_reflectionBounces = RenderSettings.reflectionBounces;
+		m_reflectionIntensity = RenderSettings.reflectionIntensity;
+		m_customReflection = RenderSettings.customReflection;
+
+		m_flareStrength = RenderSettings.flareStrength;
+		m_flareFadeSpeed = RenderSettings.flareFadeSpeed;
 	}
 #endif
 
-	[ContextMenu("Load Fog")]
-	public void LoadSetting ()
-	{
+	void _LoadFog(){
 		RenderSettings.fog = m_fogEnable;
 		RenderSettings.fogMode = m_fogMode;
 		RenderSettings.fogColor = m_fogColor;
 		RenderSettings.fogDensity = m_fogDensity;
 		RenderSettings.fogStartDistance = m_fogStartDistance;
 		RenderSettings.fogEndDistance = m_fogEndDistance;
+	}
+
+	void _LoadEnvironment(){
 		RenderSettings.skybox = m_skybox;
+		RenderSettings.sun = m_sunlight;
+		
+		RenderSettings.ambientProbe = m_ambientProbe;
+		RenderSettings.ambientLight = m_ambientLight;
+		RenderSettings.ambientGroundColor = m_ambientGroundColor;
+		RenderSettings.ambientEquatorColor = m_ambientEquatorColor;
+		RenderSettings.ambientSkyColor = m_ambientSkyColor;
+		RenderSettings.ambientMode = m_ambientMode;
+		RenderSettings.ambientIntensity = m_ambientIntensity;
+
+		RenderSettings.defaultReflectionMode = m_reflectionSource;
+		RenderSettings.defaultReflectionResolution = m_defaultReflectionResolution;
+		RenderSettings.reflectionBounces = m_reflectionBounces;
+		RenderSettings.reflectionIntensity = m_reflectionIntensity;
+		RenderSettings.customReflection = m_customReflection;
+
+		RenderSettings.flareStrength = m_flareStrength;
+		RenderSettings.flareFadeSpeed = m_flareFadeSpeed;
+	}
+
+	[ContextMenu("Load RenderSettings")]
+	public void LoadSetting ()
+	{
+		_LoadFog();
+		_LoadEnvironment();
 	}
 }
