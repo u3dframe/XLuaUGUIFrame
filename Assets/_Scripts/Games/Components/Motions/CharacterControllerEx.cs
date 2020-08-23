fb3b5d7a602c8abeeb2c39124f5235ed9e9f368a
canyons <gongyanghui1986@163.com>
2020-08-23 20:19:19 +0800

    添加一个alpha曲线的控制器

---
+++ Assets/_Scripts/Games/Components/Motions/CharacterControllerEx.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using Core.Kernel;
+
+/// <summary>
+/// 类名 : CharacterController 扩展脚本
+/// 作者 : Canyon / 龚阳辉
+/// 日期 : 2020-08-23 19:17
+/// 功能 : 
+/// </summary>
+[RequireComponent(typeof(CharacterController))]
+public class CharacterControllerEx : GobjLifeListener
+{
+	static public new CharacterControllerEx Get(GameObject gobj,bool isAdd){
+		return UtilityHelper.Get<CharacterControllerEx>(gobj,isAdd);
+	}
+
+	static public new CharacterControllerEx Get(GameObject gobj){
+		return Get(gobj,true);
+	}
+
+	public CharacterController m_c_ctrler;
+
+	void OnControllerColliderHit(ControllerColliderHit hit) {
+        // Rigidbody body = hit.collider.attachedRigidbody;
+        // if (body == null || body.isKinematic)
+        //     return;
+        
+        // if (hit.moveDirection.y < -0.3F)
+        //     return;
+        
+        // Vector3 pushDir = new Vector3(hit.moveDirection.x, 0, hit.moveDirection.z);
+        // body.velocity = pushDir * 2;
+    }
+
+	override protected void OnCall4Awake(){
+		this.csAlias = "CCtrler_Ex";
+		if(this.m_c_ctrler == null){
+			this.m_c_ctrler = this.m_gobj.GetComponentInChildren<CharacterController>(true);
+		}
+	}
+
+	override protected void OnClear(){
+		this.m_c_ctrler = null;
+	}
+
+	public void SetRadiusAndHeight(float radius,float height){
+		if(this.m_c_ctrler == null) return;
+		this.m_c_ctrler.radius = radius;
+		this.m_c_ctrler.height = height;
+		float yCenter =  (height <=  2 * radius) ? radius : height / 2;
+		this.m_c_ctrler.center = new Vector3(0,yCenter,0);
+	}
+}
---
+++ Assets/_Scripts/Games/Components/Motions/StateMachines/ClipStateMachineAlpha.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using Core.Kernel;
+
+/// <summary>
+/// 类名 : 控制动作Alpha变化的
+/// 作者 : Canyon / 龚阳辉
+/// 日期 : 2020-08-23 20:13
+/// 功能 : 
+/// </summary>
+public class ClipStateMachineAlpha : ClipStateMachine
+{
+    /// <summary>
+    /// 时间(0~1)曲线 - 控制alpha值(0~1)
+    /// </summary>
+    public AnimationCurve m_ac_alpha;
+
+    private Material m_mat;
+    private Color m_mat_color = Color.white;
+    
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        base.OnStateEnter(animator,stateInfo,layerIndex);
+
+        Renderer _rder = animator.GetComponentInChildren<Renderer>();
+        if(_rder != null){
+            m_mat = _rder.materials[0];
+            if(m_mat != null){
+                m_mat_color = m_mat.color;
+            }
+        }
+    }
+
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        base.OnStateUpdate(animator,stateInfo,layerIndex);
+
+        if (stateInfo.normalizedTime < 0 || stateInfo.normalizedTime > 1 || m_mat == null) return;
+
+        float _alpha = m_ac_alpha.Evaluate(stateInfo.normalizedTime);
+        m_mat_color.a = _alpha;
+        m_mat.color = m_mat_color;
+    }
+
+}
