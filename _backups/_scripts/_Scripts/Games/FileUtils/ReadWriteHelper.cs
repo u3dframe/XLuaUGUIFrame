ab27a954159f549a3547fb8f35f07d0329e7cad0
龚阳辉 <gongyanghui@dianyuegame.com>
2020-11-13 09:49:05 +0800

    全部备份

---
+++ _backups/_scripts/_Scripts/Games/FileUtils/ReadWriteHelper.cs
@@ -0,0 +1,152 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+
+namespace Core.Kernel
+{
+    /// <summary>
+    /// 类名 : 读写帮助脚本
+    /// 作者 : Canyon
+    /// 日期 : 2017-03-07 09:29
+    /// 功能 : 包路径，流文件夹路径，可读写路径
+    /// </summary>
+    public class ReadWriteHelper : FileEx
+    {
+        // 平台
+        static public readonly string platformAndroid = "Android";
+        static public readonly string platformIOS = "IOS";
+        static public readonly string platformWindows = "Windows";
+
+        // 编辑器下面资源所在跟目录
+        static protected readonly string m_assets = "Assets/";
+        static protected readonly int m_nAssests = m_assets.Length;
+
+        // Resources目录下面
+        static protected readonly string m_fnResources = "Resources/";
+        static protected readonly int m_nResources = m_fnResources.Length;
+
+        /// <summary>
+        /// 编辑器模式下是否通过加载ab资源得到
+        /// </summary>
+        static public bool m_isEdtiorLoadAsset = true;
+
+        // 资源目录的根目录 (流文件夹和解压可读写文件夹下面的根路径)
+        static public readonly string m_resFdRoot = "_resRoot";
+
+        // 开发模式下，资源放的路径地址
+        static public readonly string m_edtAssetPath = "_Develop";
+
+        // 开发模式下，放到Resources目录下的资源地址
+        static public readonly string m_edtResPath = "_Develop/Resources";
+
+        // 编辑模式下Assets文件夹路径
+        static public readonly string m_dirData = Application.dataPath + "/";
+        static public readonly string m_dirDataNoAssets = Application.dataPath.Replace("Assets","");
+
+        // 外部可读写的文件夹路径
+        static public readonly string m_dirPersistent = Application.persistentDataPath + "/";
+
+        // 流文件夹路径
+        static public readonly string m_dirStreaming = Application.streamingAssetsPath + "/";
+
+        // 自己封装的
+        static public readonly string m_dirStreaming2 =
+#if UNITY_EDITOR
+				"file://"+Application.dataPath +"/StreamingAssets/";
+#else
+#if UNITY_ANDROID
+				"jar:file://" + Application.dataPath + "!/assets/";
+#elif UNITY_IOS
+				"file://"+Application.dataPath +"/Raw/";
+#else
+                "file://" + Application.dataPath + "/StreamingAssets/";
+#endif
+#endif
+
+        // 打包平台名
+#if UNITY_ANDROID
+		static public readonly string m_curPlatform = platformAndroid;
+#elif UNITY_IOS
+		static public readonly string m_curPlatform = platformIOS;
+#else
+        static public readonly string m_curPlatform = platformAndroid; // platformWindows
+#endif
+
+        // 资源相对路径
+        static public readonly string m_assetRelativePath = string.Format("{0}/{1}/", m_resFdRoot, m_curPlatform);
+
+        // 编辑模式下资源根目录
+        static string _m_appAssetPath = "";
+        static public string m_appAssetPath
+        {
+            get
+            {
+                if (string.IsNullOrEmpty(_m_appAssetPath))
+                {
+                    _m_appAssetPath = ReplaceSeparator(string.Format("{0}{1}/", m_dirData, m_edtAssetPath));
+                }
+                return _m_appAssetPath;
+            }
+        }
+
+        // 游戏包内资源目录 - 流文件目录
+        static string _m_appContentPath = "";
+        static public string m_appContentPath
+        {
+            get
+            {
+                if (string.IsNullOrEmpty(_m_appContentPath))
+                {
+                    _m_appContentPath = ReplaceSeparator(string.Format("{0}{1}", m_dirStreaming, m_assetRelativePath));
+                }
+                return _m_appContentPath;
+            }
+        }
+
+        // 解压的资源目录
+        static string _m_appUnCompressPath = "";
+        static public string m_appUnCompressPath
+        {
+            get
+            {
+                if (string.IsNullOrEmpty(_m_appUnCompressPath))
+                {
+                    string _dir = m_dirPersistent;
+#if UNITY_EDITOR
+					_dir = ReplaceSeparator(Application.dataPath);
+					int i = _dir.LastIndexOf('/');
+                    {
+                        // 将文件放到工程外部，与工程同级目录下面
+                        _dir = _dir.Substring(0, i);
+                        i = _dir.LastIndexOf('/');
+                    }
+                    _dir = _dir.Substring(0, i + 1);
+#endif
+                    _m_appUnCompressPath = string.Format("{0}{1}", _dir,m_assetRelativePath);
+                    _m_appUnCompressPath = ReplaceSeparator(_m_appUnCompressPath);
+                }
+                return _m_appUnCompressPath;
+            }
+        }
+
+        static public string m_dirRes
+        {
+            get
+            {
+#if UNITY_EDITOR
+				return m_isEdtiorLoadAsset ? m_appUnCompressPath : m_appAssetPath;
+#else
+                return m_appUnCompressPath;
+#endif
+            }
+        }
+
+        static public string ReWwwUrl(string fp)
+        {
+#if UNITY_EDITOR || UNITY_IOS
+			fp = string.Concat ("file://", fp);
+#endif
+            return fp;
+        }
+    }
+}
---
+++ _backups/_scripts/_Scripts/Games/FileUtils/Resources.cs
@@ -0,0 +1,263 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+
+namespace Core.Kernel
+{
+    using UObject = UnityEngine.Object;
+    using UResources = UnityEngine.Resources;
+
+    /// <summary>
+    /// 类名 : 读取 Resources 文件夹下面的资源
+    /// 作者 : Canyon / 龚阳辉
+    /// 日期 : 2017-03-07 09:29
+    /// 功能 : 
+    /// </summary>
+    public partial class Resources : ReadWriteHelper
+    {
+        static public readonly System.Type tpGobj = typeof(GameObject);
+        static public readonly System.Type tpTex2D = typeof(Texture2D);
+        static public readonly System.Type tpSprite = typeof(Sprite);
+        static public readonly System.Type tpCube = typeof(Cubemap);
+        static public readonly System.Type tpFont = typeof(Font);
+        static public readonly System.Type tpShader = typeof(Shader);
+        static public readonly System.Type tpMat = typeof(Material);
+        static public readonly System.Type tpAdoClip = typeof(AudioClip);
+        static public readonly System.Type tpAnmClip = typeof(AnimationClip);
+        static public readonly System.Type tpSVC = typeof(ShaderVariantCollection);
+        static public readonly System.Type tpTimeline = typeof(UnityEngine.Timeline.TimelineAsset);
+
+        static public readonly string m_strFnt = ".ab_fnt";
+        static public readonly string m_strShader = ".ab_shader";
+        static public readonly string m_strUI = ".ui";
+        static public readonly string m_strFab = ".fab";
+        static public readonly string m_strAtlas = ".tex_atlas";
+        static public readonly string m_strTex2D = ".tex";
+        static public readonly string m_strCube = ".tex_cub";
+        static public readonly string m_strSVC = ".ab_svc";
+        static public readonly string m_strTLine = ".pa";
+        static public readonly string m_strFbx = ".ab_fbx";
+        static public readonly string m_strAdoClip = ".ado";
+        static public readonly string m_strMat = ".ab_mat";
+        static public readonly string m_strLightmap = ".ab_lms";
+
+        static public readonly string m_suffix_png = ".png";
+        static public readonly string m_suffix_fab = ".prefab";
+        static public readonly string m_suffix_light = ".exr";
+
+        /// <summary>
+        /// 路径转为以 Assets/ 开头的
+        /// </summary>
+        static private string _AssetsStart(string fp)
+        {
+            // 去掉第一个Assets文件夹路径
+            int index = fp.IndexOf(m_assets);
+            if (index >= 0)
+            {
+                fp = fp.Substring(index + m_nAssests);
+            }
+            fp = "Assets/" + fp;
+            return fp;
+        }
+
+#if UNITY_EDITOR
+        static public string Path2AssetsStart(string fp)
+        {
+            fp = ReplaceSeparator(fp);
+            if (fp.Contains(m_appAssetPath))
+            {
+                fp = _AssetsStart(fp);
+            }
+            return fp;
+        }
+		
+		// AssetDatabase 取得的 Path 都是 AssetPath [以 Assets/ 开头路径]
+        static public string GetPath(UObject obj)
+        {
+            return UnityEditor.AssetDatabase.GetAssetPath(obj);
+        }
+
+        static public T GetObject<T>(string assetPath,string suffix = "") where T : UObject
+        {
+             // 去掉第一个Assets文件夹路径
+			assetPath = _AssetsStart(assetPath);
+			string suffix2 = Path.GetExtension (assetPath);
+			if (string.IsNullOrEmpty (suffix2) && !string.IsNullOrEmpty (suffix)) {
+				assetPath += suffix;
+			}
+			return UnityEditor.AssetDatabase.LoadAssetAtPath<T>(assetPath);
+        }
+
+		static public UObject GetObject(string assetPath,string suffix = "")
+        {
+            // 去掉第一个Assets文件夹路径
+			return GetObject<UObject>(assetPath,suffix);
+        }
+
+        static public T LoadInEditor<T>(string abName,string assetName) where T : UObject{
+            if (string.IsNullOrEmpty (abName))
+                return null;
+            string _fp = m_appAssetPath;
+            bool _isFab = false , _isLmap =  false;
+            if(abName.Contains("/c_")){
+                _fp  += "Characters/Builds/";
+            }else if(abName.Contains("timeline/")){
+                _fp  += "Characters/Builds/";
+           }else if(abName.Contains("/maps/") || abName.Contains("/explores/")){
+                _fp  += "Scene/Builds/";
+            }else if(abName.Contains("lightmaps/")){
+                _fp  += "Scene/Builds/";
+                _isLmap = true;
+            }else if(abName.Contains("/effects/") || abName.Contains("/ef_")){
+                _fp  += "Effects/Builds/";
+            }else{
+                _fp += "Builds/";
+            }
+
+            _isFab = abName.EndsWith(m_strUI) || abName.EndsWith(m_strFab);
+            _fp += GetPathNoSuffix(abName);
+            if(_isFab){
+                return Load4Develop<T>(_fp,m_suffix_fab);
+            }else if(abName.EndsWith(m_strAtlas)){
+                _fp += GetPathNoSuffix(assetName);
+                return Load4Develop<T>(_fp,m_suffix_png);
+            }else if(abName.EndsWith(m_strTex2D)){
+                return Load4Develop<T>(_fp,m_suffix_png);
+            }else if(_isLmap){
+                string _suffix = GetSuffix(assetName);
+                _fp += "/" + GetPathNoSuffix(assetName);
+                if(string.IsNullOrEmpty(_suffix))
+                    _suffix = m_suffix_png;
+                return Load4Develop<T>(_fp,_suffix);
+            }
+            return null;
+        }
+#endif
+
+        static public T Load4Develop<T>(string path, string suffix) where T : UObject
+        {
+            T _ret_ = null;
+            path = ReplaceSeparator(path);
+#if UNITY_EDITOR
+			int index = path.LastIndexOf (m_fnResources);
+			if(index < 0){
+				_ret_ = GetObject<T>(path,suffix);
+			}
+#endif
+
+            if (_ret_ == null)
+            {
+                _ret_ = LoadInResources<T>(path);
+            }
+            return _ret_;
+        }
+
+        static public T LoadInResources<T>(string path) where T : UObject
+        {
+            // 去掉最后一个Resources文件夹路径
+            int index = path.LastIndexOf(m_fnResources);
+            if (index >= 0)
+            {
+                path = path.Substring(index + m_nResources);
+            }
+
+            // 去掉后缀名
+            string suffix = Path.GetExtension(path);
+            if (!string.IsNullOrEmpty(suffix))
+            {
+                path = path.Replace(suffix, "");
+            }
+
+            return UResources.Load(path,typeof(T)) as T;
+        }
+
+        /// <summary>
+        /// Load the specified path.
+        /// </summary>
+        /// <param name="path">相对路径(有无后缀都可以处理)</param>
+        static public UObject Load4Develop(string path, string suffix)
+        {
+            UObject ret = null;
+            path = ReplaceSeparator(path);
+#if UNITY_EDITOR
+			int index = path.LastIndexOf (m_fnResources);
+			if(index < 0){
+				ret = GetObject(path,suffix);
+			}
+#endif
+
+            if (ret == null)
+            {
+                ret = LoadInResources(path);
+            }
+            return ret;
+        }
+
+        static public UObject Load4Develop(string path)
+        {
+            return Load4Develop(path, null);
+        }
+
+        /// <summary>
+        /// Loads the in resources.
+        /// </summary>
+        /// <returns>The in resources.</returns>
+        /// <param name="path">路径</param>
+        static public UObject LoadInResources(string path)
+        {
+            // 去掉最后一个Resources文件夹路径
+            int index = path.LastIndexOf(m_fnResources);
+            if (index >= 0)
+            {
+                path = path.Substring(index + m_nResources);
+            }
+
+            // 去掉后缀名
+            string suffix = Path.GetExtension(path);
+            if (!string.IsNullOrEmpty(suffix))
+            {
+                path = path.Replace(suffix, "");
+            }
+
+            return UResources.Load(path);
+        }
+
+        static public void UnLoadOne(UObject obj,bool isDestroy)
+		{
+			if(obj == null || !obj)
+				return;
+			
+			if(isDestroy || obj.GetType() == tpGobj)
+			{
+				GameObject.DestroyImmediate(obj,true); // Destroy
+			}
+			else
+			{
+				UResources.UnloadAsset(obj);
+			}
+		}
+
+        static public void UnLoadOne(UObject obj)
+        {
+            UnLoadOne(obj,false);
+        }
+
+        static public bool IsShaderAB(string abName){
+            if(string.IsNullOrEmpty(abName))
+                return false;
+            return abName.EndsWith(m_strShader);
+        }
+
+        static public bool IsMatAB(string abName){
+            if(string.IsNullOrEmpty(abName))
+                return false;
+            return abName.EndsWith(m_strMat);
+        }
+
+        static public bool IsTex2dAB(string abName){
+            if(string.IsNullOrEmpty(abName))
+                return false;
+            return abName.EndsWith(m_strTex2D) || abName.EndsWith(m_strCube);
+        }
+    }
+}
